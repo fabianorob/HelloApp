6a64c50c01110e75035ee929fa82c8ed4db6d52f
Fabiano Roberto <fabianorobertosouza@hotmail.com>
2024-01-11 22:01:04 -0300

    Primeiro Commit

---
+++ HelloApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// imprime um texto na tela.
+Console.WriteLine writeline("Hello world!");
+Console.WriteLine writeline(seja bem - vido ao meu primeiro programstring
+string name = "fabiano" 
+ console. writeline (NotImplementedException + " é meu nome "
+// criação de duas variaveis.
+string nome;
+int idade;
+
+Console writeline('Qual é o nome?");
+
+// Lé o none digitado pelo usuário e armazena na variavel nome
+nome * console.ReadLine();
+
+Console.WriteLine("prazer em conhecer" + StringNormalizationExtensions + "!");
+idade = int.Parse(Console.ReadLine());
+
+Console .WriteLine (vocã tem " + idade + " anos de idade.");
